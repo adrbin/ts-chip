755e549b87a1efdc023955e531f742323e039bd8
adrbin <35666777+adrbin@users.noreply.github.com>
2022-09-22 18:16:37 +0200

    Add PWA support

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,8 @@
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open('sw-cache').then(function (cache) {
-      return cache.addAll(['index.html', 'pong.js']);
-    })
+      return cache.addAll(['index.html', 'web/ts-chip-web.ts']);
+    }),
   );
 });
 
@@ -10,6 +10,6 @@ self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
-    })
+    }),
   );
 });
